8ec4cf8328c4500a61cdd30df2a6e1a441dafd0c
Arianna Johnson <ariannajohnson588@gmail.com>
2020-12-30 18:05:45 -0500

    more styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  
+  const history = useHistory();
+
+
   const openMenu = () => {
     if (showMenu) return;
     setShowMenu(true);
   };
-  
+
+
   useEffect(() => {
     if (!showMenu) return;
 
@@ -19,14 +23,17 @@ function ProfileButton({ user }) {
     };
 
     document.addEventListener('click', closeMenu);
-  
+
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    history.push("/");
   };
+
+
 
   return (
     <>
